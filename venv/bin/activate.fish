bd6c7bb498ec0148b01125f93ed2477ed89633bd
José Vinícius Silva Ferreira <ferreiravini468@gmail.com>
2020-06-20 03:03:36 -0300

    Testes

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/viniciusilva/Documentos/Repositórios/python/venv"
+set -gx VIRTUAL_ENV "/home/viniciusilva/Documentos/GitHub/python/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
