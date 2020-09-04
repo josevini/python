f250398aa7d151200698bd7af82f4313a34dc3b3
José Vinícius Silva <ferreiravini468@gmail.com>
2020-09-03 22:05:46 -0300

    Update ex011.py

--- Livro de Python/capitulo3/ex011.py
+++ Livro de Python/capitulo3/ex011.py
@@ -1,1 +1,6 @@
 # Exercício 3.15 - Livro
+import math
+cigarros = int(input('Quantos cigarros você fuma por dia? '))
+anos = int(input('Há quantos anos você fuma? '))
+dias_perdidos = ((cigarros * 10) * (anos * 3650) / 1440)
+print(f'No total, você perdeu, aproximadamente, {math.ceil(dias_perdidos)} dias.')
