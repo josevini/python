ab1098eb03ddeaf377c39eefb9a0af28997550c0
José Vinícius Silva <ferreiravini468@gmail.com>
2020-11-04 15:13:21 -0300

    Update ex36.py

--- Livro de Python/capitulo5/ex36.py
+++ Livro de Python/capitulo5/ex36.py
@@ -1,17 +1,20 @@
-valor = int(input('Digite o valor a pagar: '))
+# Exercício 5.18 - Livro
+valor = int(input('Valor para saque: '))
 ced = 0
-ced_atual = 50
-apagar = valor
+ced_atual = 100
+saque = valor
 while True:
-    if ced_atual <= apagar:
-        apagar -= ced_atual
+    if saque >= ced_atual:
+        saque -= ced_atual
         ced += 1
     else:
         if ced != 0:
             print(f'{ced} cédulas de R${ced_atual}')
-        if apagar == 0:
+        if saque == 0:
             break
-        if ced_atual == 50:
+        if ced_atual == 100:
+            ced_atual = 50
+        elif ced_atual == 50:
             ced_atual = 20
         elif ced_atual == 20:
             ced_atual = 10
