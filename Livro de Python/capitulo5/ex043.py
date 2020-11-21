5cc5e0cd704c0bad7df54844b6be3a316bd77ed5
José Vinícius Silva <ferreiravini468@gmail.com>
2020-11-21 15:47:57 -0300

    Create ex043.py

---
+++ Livro de Python/capitulo5/ex043.py
@@ -0,0 +1,17 @@
+dividendo = int(input('Digite o dividendo: '))
+divisor = int(input('Digite o divisor: '))
+
+div = dividendo
+cont = 0
+while True:
+    div = div - divisor
+    cont = cont + 1
+    if div == 0:
+        resultado = cont
+        resto = 0
+        break
+    elif div < 0:
+        resultado = cont - 1
+        resto = div + divisor
+        break
+print(f'Resto da divisão: {resto}')
