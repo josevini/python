e47a0cf33c185f3bf6b30d2f76b7f967761f2e30
José Vinícius Silva <ferreiravini468@gmail.com>
2020-11-21 21:55:55 -0300

    Update ex44.py

--- Livro de Python/capitulo6/ex44.py
+++ Livro de Python/capitulo6/ex44.py
@@ -1,3 +1,10 @@
 # Exercício 6.1 - Livro
-
-
+notas = []
+cont = soma = 0
+while cont < 7:
+    num = float(input(f'Nota {cont + 1}: '))
+    notas.append(num)
+    soma += notas[cont]
+    cont += 1
+media = soma / cont
+print(f'{media:.2f}')
