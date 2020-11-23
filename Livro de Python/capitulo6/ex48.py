1ba402171f1a065c8ceebb316fca509d8e0f5a18
José Vinícius Silva <ferreiravini468@gmail.com>
2020-11-23 15:17:23 -0300

    Update ex48.py

--- Livro de Python/capitulo6/ex48.py
+++ Livro de Python/capitulo6/ex48.py
@@ -2,6 +2,7 @@
 
 ultimo = 10
 fila = list(range(1, ultimo + 1))
+sair = False
 while True:
     print('=-=' * 20)
     print(f'Existem {len(fila)} clientes na fila')
@@ -9,16 +10,22 @@ while True:
     print('Digite F para adicionar um cliente ao fim da fila')
     print('ou A para realizar o atendimento. S para sair.')
     op = str(input('Operação: '))
-    if op == 'S' or op == 's':
-        break
-    elif op == 'F' or op == 'f':
-        ultimo += 1
-        fila.append(ultimo)
-    elif op == 'A' or op == 'a':
-        if len(fila) > 0:
-            atendido = fila.pop(0)
-            print(f'Cliente {atendido} atendido')
+    tot = len(op)
+    pos = 0
+    while pos < tot:
+        if op[pos] == 'S' or op[pos] == 's':
+            sair = True
+        elif op[pos] == 'F' or op[pos] == 'f':
+            ultimo += 1
+            fila.append(ultimo)
+        elif op[pos] == 'A' or op[pos] == 'a':
+            if len(fila) > 0:
+                atendido = fila.pop(0)
+                print(f'Cliente {atendido} atendido')
+            else:
+                print('Fila vazia')
         else:
-            print('Fila vazia')
-    else:
-        print('Informe uma operação válida!')
+            print('Informe uma operação válida!')
+        pos += 1
+    if sair:
+        break
