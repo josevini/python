4553e393e43cddd3c74d2f75e024b40a64cf8e42
José Vinícius Silva <62161557+josevini@users.noreply.github.com>
2020-12-18 18:44:30 -0300

    Update ex83.py

--- Livro de Python/capitulo8/ex83.py
+++ Livro de Python/capitulo8/ex83.py
@@ -4,13 +4,16 @@ import random
 comp = random.randint(1, 10)
 tentativas = 3
 while tentativas > 0:
-    print('---' * 10)
-    print(f'{tentativas}', 'tentativas restantes' if tentativas > 1 else 'tentativa restante')
-    num = int(input('Digite um valor entre 1 e 10: '))
-    print('---' * 10)
-    if num == comp:
-        print('Você acertou!')
-        break
-    else:
-        print('Você não acertou!')
-    tentativas -= 1
+    try:
+        print('---' * 10)
+        print(f'{tentativas}', 'tentativas restantes' if tentativas > 1 else 'tentativa restante')
+        num = int(input('Digite um valor entre 1 e 10: '))
+        print('---' * 10)
+        if num == comp:
+            print('Você acertou!')
+            break
+        else:
+            print('Você não acertou!')
+        tentativas -= 1
+    except Exception as excecao:
+        print(f'Alerta de exceção: {excecao}')
