c9d6f4f2af3122d72ca3dcda2ff2992ab6b7f95a
José Vinícius Silva <62161557+josevini@users.noreply.github.com>
2020-12-18 18:41:17 -0300

    Create ex83.py

---
+++ Livro de Python/capitulo8/ex83.py
@@ -0,0 +1,16 @@
+# Exercício 8.13 - Livro
+
+import random
+comp = random.randint(1, 10)
+tentativas = 3
+while tentativas > 0:
+    print('---' * 10)
+    print(f'{tentativas}', 'tentativas restantes' if tentativas > 1 else 'tentativa restante')
+    num = int(input('Digite um valor entre 1 e 10: '))
+    print('---' * 10)
+    if num == comp:
+        print('Você acertou!')
+        break
+    else:
+        print('Você não acertou!')
+    tentativas -= 1
