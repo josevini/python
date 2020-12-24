d28001d85acfb2a7f7f4353f04f4e8db86efefa0
José Vinícius Silva <62161557+josevini@users.noreply.github.com>
2020-12-24 15:58:20 -0300

    Update func.py

--- func.py
+++ func.py
@@ -14,3 +14,22 @@ def toBinary(num):
         pot -= 1
     res = soma
     return res
+
+def organiza(lista=[]):
+    cont = 0
+    nova_lista = []
+    for valor in lista:
+        cont += 1
+        if cont == 1:
+            nova_lista.append(valor)
+        else:
+            if valor < min(nova_lista):
+                nova_lista.insert(0, valor)
+            elif valor > max(nova_lista):
+                nova_lista.append(valor)
+            else:
+                for pos in range(0, len(nova_lista) + 1):
+                    if valor not in nova_lista:
+                        if valor < nova_lista[pos]:
+                            nova_lista.insert(pos, valor)
+    return nova_lista
