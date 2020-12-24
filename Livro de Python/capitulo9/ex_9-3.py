3a4b222241b331e6741b69220385cd0ad31b68a1
José Vinícius Silva <62161557+josevini@users.noreply.github.com>
2020-12-24 15:57:59 -0300

    Create ex_9-3.py

---
+++ Livro de Python/capitulo9/ex_9-3.py
@@ -0,0 +1,7 @@
+# Exercício 9.3 - Livro
+# Falta terminar
+
+with open('parimpar.txt', 'w') as parImpar, open('pares.txt', 'r') as pares, open('impares.txt', 'r') as impares:
+    i = impares.readline()
+    p = pares.readline()
+    numeros = []
