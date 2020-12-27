9a0878fcff3eafa15e1734ee9574d15d76106df9
José Vinícius Silva <62161557+josevini@users.noreply.github.com>
2020-12-27 20:42:41 -0300

    Update ex_9-5.py

--- Introdução à Programação/capitulo9/ex_9-5.py
+++ Introdução à Programação/capitulo9/ex_9-5.py
@@ -1,3 +1,8 @@
 # Exercício 9.5 - Livro
 
-
+with open('pares.txt', 'r') as pares:
+    valores_pares = pares.readlines()
+    ultimo = len(valores_pares) - 1
+    primeiro = -1
+    for pos in range(ultimo, primeiro, -1):
+        print(f'{valores_pares[pos]}', end='')
