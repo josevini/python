7590efacb68a77628a7c09bcdb0390b9ebd371a9
José Vinícius Silva <62161557+josevini@users.noreply.github.com>
2020-12-27 21:56:52 -0300

    update ex_9-6.py

--- Introdução à Programação/capitulo9/ex_9-6.py
+++ Introdução à Programação/capitulo9/ex_9-6.py
@@ -2,4 +2,19 @@
 
 with open('txt/entrada.txt', 'r') as entrada:
     linhas = entrada.readlines()
-    print(linhas)
+    for linha in linhas:
+        if linha[0] == ';':
+            continue
+        elif linha[0] == '>':
+            print(linha[1:].rjust(79))
+        elif linha[0] == '<':
+            print(linha[1:].ljust(79))
+        elif linha[0] == '*':
+            print(linha[1:].center(79))
+        elif linha[0] == '=':
+            print('=' * 40)
+        elif linha[0] == '.':
+            input('Digite algo para continuar ')
+            print()
+        else:
+            print(linha)
