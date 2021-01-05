c6de8c60b907def5dd4618dc344028207de88d13
José Vinícius Silva <62161557+josevini@users.noreply.github.com>
2021-01-05 00:12:53 -0300

    Update ex_9-7.py

--- Introdução à Programação/capitulo9/ex_9-7.py
+++ Introdução à Programação/capitulo9/ex_9-7.py
@@ -1,1 +1,21 @@
 # Exercício 9.7 - Livro
+from functions import *
+
+with open('txt/arquivo_paginas.txt', encoding='UTF-8') as file:
+    filename = getFilename(file.name)
+    text = file.read().split()
+    lineCount = pageCount = 1
+    line = msg = ''
+    maxLength = 76
+    with open('txt/novo_arquivo.txt', 'w', encoding='UTF-8') as newFile:
+        for word in text:
+            msg = f'{word} '
+            if maxLength - len(line) >= len(msg):
+                line += msg
+            else:
+                newFile.write(f'{line}\n')
+                lineCount += 1
+                if lineCount % 60 == 0:
+                    newFile.write(f' == Página {pageCount} → {filename} == \n')
+                    pageCount += 1
+                line = f'{msg}'
