b22b186ca9821caea705ece1d60cdf31d4e99c8b
José Vinícius Silva <62161557+josevini@users.noreply.github.com>
2021-01-05 15:12:39 -0300

    Update ex_9-8.py

--- Introdução à Programação/capitulo9/ex_9-8.py
+++ Introdução à Programação/capitulo9/ex_9-8.py
@@ -1,3 +1,24 @@
-# Exercício 9.8 - Livro
+# Exercício 9. - Livro
 
+from functions import *
+
+maxLength = int(input('Insira o limite de letras por linha: '))
+lineLimit = int(input('Insira o limite de linhas por páginas: '))
+with open('txt/arquivo_paginas.txt', encoding='UTF-8') as file:
+    filename = getFilename(file.name)
+    text = file.read().split()
+    lineCount = pageCount = 1
+    line = msg = ''
+    with open('txt/novo_arquivo.txt', 'w', encoding='UTF-8') as newFile:
+        for word in text:
+            msg = f'{word} '
+            if maxLength - len(line) >= len(msg):
+                line += msg
+            else:
+                newFile.write(f'{line}\n')
+                lineCount += 1
+                if lineCount % lineLimit == 0:
+                    newFile.write(f' == Página {pageCount} → {filename} == \n')
+                    pageCount += 1
+                line = f'{msg}'
 
