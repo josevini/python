7febfdd224e60d4009c812f13a9d630ca5e8de4e
José Vinícius Silva <62161557+josevini@users.noreply.github.com>
2021-01-08 21:53:08 -0300

    Update ex_9-13.py

--- Introdução à Programação/capitulo9/ex_9-13.py
+++ Introdução à Programação/capitulo9/ex_9-13.py
@@ -1,3 +1,24 @@
 # Exercício 9.13 - Livro
 
+import sys
 
+args = sys.argv
+if len(args) != 4:
+    print('Formato: [filename] [start] [end]')
+else:
+    filename = args[1]
+    start = args[2]
+    end = args[3]
+    if start < end:
+        try:
+            file = open(f'txt/{filename}', 'r', encoding='utf-8')
+            content = file.readlines()
+            for line in range(int(start), int(end)):
+                print(f'{content[line]}')
+            file.close()
+        except FileNotFoundError:
+            print('Arquivo não encontrado!')
+        except IndexError:
+            print('Fim da lista.')
+    else:
+        print('Início não pode ser maior que o fim.')
