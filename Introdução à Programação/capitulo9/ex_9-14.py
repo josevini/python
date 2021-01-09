cdbc212fed543a887235b325016302c8da99a6c7
José Vinícius Silva <62161557+josevini@users.noreply.github.com>
2021-01-09 17:33:55 -0300

    Update ex_9-14.py

--- Introdução à Programação/capitulo9/ex_9-14.py
+++ Introdução à Programação/capitulo9/ex_9-14.py
@@ -1,1 +1,14 @@
 # Exercício 9.14 - Livro
+
+import sys
+
+args = sys.argv
+if len(args) != 2:
+    print('Formato: [filename]')
+else:
+    filename = args[1]
+    path = f'txt/{filename}'
+    file = open(path, 'r', encoding='utf-8')
+    content = file.read()
+    print(content)
+    file.close()
