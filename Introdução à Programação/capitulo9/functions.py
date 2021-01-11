2e4bbc53d302b4bd9878cf63ce413ec1e390674f
José Vinícius Silva <62161557+josevini@users.noreply.github.com>
2021-01-10 22:33:08 -0300

    Update functions.py

--- Introdução à Programação/capitulo9/functions.py
+++ Introdução à Programação/capitulo9/functions.py
@@ -2,3 +2,30 @@
 def getFilename(path):
     filename = path.split('/')[-1]
     return filename
+
+# Exercício 9.14
+def removeSpaces(string=''):
+    spaces = 0
+    newString = ''
+    for char in string:
+        if char != ' ':
+            newString += char
+            spaces = 0
+        else:
+            if spaces < 1:
+                newString += char
+                spaces += 1
+    return newString
+
+def removeEmptyLine(content=''):
+    emptyLine = 0
+    newString = ''
+    for line in content:
+        if line != '\n':
+            newString += line
+            emptyLine = 0
+        else:
+            if emptyLine < 1:
+                newString += line
+                emptyLine += 1
+    return newString
