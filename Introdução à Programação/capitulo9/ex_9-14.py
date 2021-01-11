9ae583dfe8d2100c26c308e93e1aeff6d7ddb502
José Vinícius Silva <62161557+josevini@users.noreply.github.com>
2021-01-10 22:33:11 -0300

    Update ex_9-14.py

--- Introdução à Programação/capitulo9/ex_9-14.py
+++ Introdução à Programação/capitulo9/ex_9-14.py
@@ -1,6 +1,7 @@
 # Exercício 9.14 - Livro
 
 import sys
+from functions import removeSpaces, removeEmptyLine
 
 args = sys.argv
 if len(args) != 2:
@@ -9,6 +10,5 @@ else:
     filename = args[1]
     path = f'txt/{filename}'
     file = open(path, 'r', encoding='utf-8')
-    content = file.read()
-    print(content)
+    content = removeSpaces(removeEmptyLine(file.readlines()))
     file.close()
