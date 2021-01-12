062f5f15524db332a87c785112d8bad34cfca90f
José Vinícius Silva <62161557+josevini@users.noreply.github.com>
2021-01-12 18:55:03 -0300

    Update ambiente.py

--- Introdução à Programação/capitulo9/ambiente.py
+++ Introdução à Programação/capitulo9/ambiente.py
@@ -24,3 +24,83 @@ def novo():
     nome = pedeNome()
     telefone = pedeTelefone()
     agenda.append([nome, telefone])
+
+def apaga():
+    global agenda
+    nome = pedeNome()
+    p = pesquisa(nome)
+    if p is not None:
+        del agenda[p]
+    else:
+        print('Nome não encontrado!')
+
+def altera():
+    p = pesquisa(pedeNome())
+    if p is not None:
+        nome = agenda[p][0]
+        telefone = agenda[p][1]
+        print('Encontrado: ')
+        mostraDados(nome, telefone)
+        nome = pedeNome()
+        telefone = pedeTelefone()
+        agenda[p] = [nome, telefone]
+    else:
+        print('Nome não encontrado!')
+
+def lista():
+    print('\nAgenda\n\n-------')
+    for e in agenda:
+        mostraDados(e[0], e[1])
+    print('-------\n')
+
+def lê():
+    global agenda
+    nomeArquivo = pedeNomeArquivo()
+    with open(nomeArquivo, 'r', encoding='utf-8') as arquivo:
+        agenda = []
+        for l in arquivo.readlines():
+            nome, telefone = l.strip().split('#')
+            agenda.append([nome, telefone])
+
+def grava():
+    nomeArquivo = pedeNomeArquivo()
+    with open(nomeArquivo, 'w', encoding='utf-8') as arquivo:
+        for e in agenda:
+            arquivo.write(f'{e[0]}#{e[1]}\n')
+
+def validaFaixaInteiro(pergunta, inicio, fim):
+    while True:
+        try:
+            valor = int(input(pergunta))
+            if inicio <= valor <= fim:
+                return valor
+        except ValueError:
+            print(f'Valor inválido, favor digitar entre {inicio} e {fim}')
+
+def menu():
+    print("""
+    1 - Novo
+    2 - Altera
+    3 - Apaga
+    4 - Lista
+    5 - Grava
+    6 - Lê
+    0 - Sai    
+    """)
+    return validaFaixaInteiro('Escolha uma opção: ', 0, 6)
+    while True:
+        opcao = menu()
+        if opcao == 0:
+            break
+        elif opcao == 1:
+            novo()
+        elif opcao == 2:
+            altera()
+        elif opcao == 3:
+            apaga()
+        elif opcao == 4:
+            lista()
+        elif opcao == 5:
+            grava()
+        elif opcao == 6:
+            lê()
