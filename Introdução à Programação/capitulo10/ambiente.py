ecdd97592f7aa4ae63bb6418a2278df08e848073
José Vinícius Silva <ferreiravini468@gmail.com>
2021-01-23 18:09:29 -0300

    Update ambiente.py

--- Introdução à Programação/capitulo10/ambiente.py
+++ Introdução à Programação/capitulo10/ambiente.py
@@ -1,7 +1,11 @@
 from Clientes import Cliente
+from Contas import Conta
 
 vinicius = Cliente('Vinicius', '9640-2432')
 print(f'Nome: {vinicius.nome} | Telefone: {vinicius.telefone}')
 
 niara = Cliente('Niara', '8539-1321')
 print(f'Nome: {niara.nome} | Telefone: {niara.telefone}')
+
+minhaConta = Conta(vinicius, '2305', 600)
+minhaConta.resumo()
