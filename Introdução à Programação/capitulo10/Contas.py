450368d4cef0101481d0734691701711918d1be1
José Vinícius Silva <ferreiravini468@gmail.com>
2021-01-23 18:09:20 -0300

    Update Contas.py

--- Introdução à Programação/capitulo10/Contas.py
+++ Introdução à Programação/capitulo10/Contas.py
@@ -4,7 +4,7 @@ class Conta:
         self.clientes = clientes
         self.numero = numero
     def resumo(self):
-        print(f'CC Número: {self.numero} Saldo: {self.saldo:10.2f}')
+        print(f'CC Número: {self.numero} Saldo: {self.saldo:.2f}')
     def saque(self, valor):
         if self.saldo >= valor:
             self.saldo -= valor
