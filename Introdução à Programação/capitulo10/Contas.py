477269a139a9e33998e2ce8f2befde99a4f28e82
José Vinícius Silva <ferreiravini468@gmail.com>
2021-01-23 18:17:31 -0300

    Update Contas.py

--- Introdução à Programação/capitulo10/Contas.py
+++ Introdução à Programação/capitulo10/Contas.py
@@ -7,8 +7,12 @@ class Conta:
         print(f'CC Número: {self.numero} Saldo: {self.saldo:.2f}')
     def saque(self, valor):
         if self.saldo >= valor:
+            print('Fazendo saque!')
             self.saldo -= valor
+        else:
+            print('Saldo insuficiente!')
     def deposito(self, valor):
         self.saldo += valor
+        print('Fazendo depósito!')
 
 
