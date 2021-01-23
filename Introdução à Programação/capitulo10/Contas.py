724f1e59bf5f04c3f1015ed4e45d6e76596ed856
José Vinícius Silva <ferreiravini468@gmail.com>
2021-01-23 17:05:26 -0300

    Create Contas.py

---
+++ Introdução à Programação/capitulo10/Contas.py
@@ -0,0 +1,14 @@
+class Conta:
+    def __init__(self, clientes, numero, saldo=0):
+        self.saldo = saldo
+        self.clientes = clientes
+        self.numero = numero
+    def resumo(self):
+        print(f'CC Número: {self.numero} Saldo: {self.saldo:10.2f}')
+    def saque(self, valor):
+        if self.saldo >= valor:
+            self.saldo -= valor
+    def deposito(self, valor):
+        self.saldo += valor
+
+
