7a317a119d1ffd46cc767d681f1f81aba1916c5f
José Vinícius Silva <ferreiravini468@gmail.com>
2021-01-24 15:26:48 -0300

    Create ex_10-9.py

---
+++ Introdução à Programação/capitulo10/ex_10-9.py
@@ -0,0 +1,28 @@
+class Estado:
+    def __init__(self, nome, sigla, cidades):
+        self.nome = nome
+        self.sigla = sigla
+        self.cidades = cidades
+    def populacaoTotal(self):
+        popTotal = 0
+        for cidade in self.cidades:
+            popTotal += cidade.populacao
+        print(f'{self.nome} tem um total de {popTotal} habitantes')
+    def populacaoIndividual(self):
+        print('-' * 25)
+        for cidade in self.cidades:
+            print(f'{cidade.nome:<16} {cidade.populacao}')
+        print('-' * 25)
+
+class Cidade:
+    def __init__(self, nome, populacao):
+        self.nome = nome
+        self.populacao = populacao
+
+maceio = Cidade('Maceió', 1_038_948)
+arapiraca = Cidade('Arapiraca', 231_747)
+rio_largo = Cidade('Rio Largo', 75_120)
+
+al = Estado('Alagoas', 'AL', [maceio, arapiraca, rio_largo])
+al.populacaoIndividual()
+al.populacaoTotal()
