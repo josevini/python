ffa2e078def5d455d2bbd79dcd0a765304713731
José Vinícius Silva <ferreiravini468@gmail.com>
2021-01-25 16:35:07 -0300

    Create ex_10-11.py

---
+++ Introdução à Programação/capitulo10/ex_10-11.py
@@ -0,0 +1,55 @@
+# Exercício 10.11 - Livro
+
+class Conta:
+    def __init__(self, clientes, numero, saldo=0):
+        self.saldo = 0
+        self.clientes = clientes
+        self.numero = numero
+        self.operacoes = []
+        self.deposito(saldo)
+    def resumo(self):
+        print('--' * 16)
+        print(f'Dono(s) da conta: ', end='')
+        try:
+            for cliente in self.clientes:
+                print(cliente.nome, end=' ')
+            print('')
+        except TypeError:
+            print(self.clientes.nome)
+        print(f'CC Número: {self.numero} Saldo: {self.saldo:.2f}')
+    def saque(self, valor):
+        if self.saldo >= valor:
+            self.saldo -= valor
+            self.operacoes.append(['SAQUE', valor])
+            return True
+        else:
+            return False
+    def deposito(self, valor):
+        self.saldo += valor
+        self.operacoes.append(['DEPÓSITO', valor])
+    def extrato(self):
+        print('-' * 20)
+        print(f'Extrato CC N° {self.numero}\n')
+        for o in self.operacoes:
+            print(f'{o[0]:10s} {o[1]:5.2f}')
+        print('-' * 20)
+
+class ContaEspecial(Conta):
+    def __init__(self, clientes, numero, saldo=0, limite=0):
+        Conta.__init__(self, clientes, numero, saldo)
+        self.limite = limite
+    def saque(self, valor):
+        if self.saldo + self.limite >= valor:
+            self.saldo -= valor
+            self.operacoes.append(['SAQUE', valor])
+            return True
+        else:
+            return False
+    def extrato(self):
+        print('-' * 20)
+        print(f'Extrato CE N° {self.numero}')
+        print(f'Limite: R${self.limite} Total para saque: R$: {self.limite + self.saldo}')
+        for o in self.operacoes:
+            print(f'{o[0]:10s} {o[1]:5.2f}')
+        print('-' * 20)
+
