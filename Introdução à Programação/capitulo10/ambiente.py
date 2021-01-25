670c65f6a67aabfa085f6f515b8775ffa9c15ffa
José Vinícius Silva <ferreiravini468@gmail.com>
2021-01-25 19:30:00 -0300

    Update ambiente.py

---
+++ Introdução à Programação/capitulo10/ambiente.py
@@ -0,0 +1,28 @@
+class listaUnica:
+    def __init__(self, elem_class):
+        self.lista = []
+        self.elem_class = elem_class
+    def __len__(self):
+        return len(self.lista)
+    def __iter__(self):
+        return iter(self.lista)
+    def __getitem__(self, p):
+        return self.lista[p]
+    def indiceValido(self, i):
+        return i >= 0 and i < len(self.lista)
+    def adiciona(self, elem):
+        if self.pesquisa(elem) == -1:
+            self.lista.append(elem)
+    def remove(self, elem):
+        self.lista.remove(elem)
+    def pesquisa(self, elem):
+        self.verificaTipo(elem)
+        try:
+            return self.lista.index(elem)
+        except ValueError:
+            return -1
+    def verificaTipo(self, elem):
+        if not isinstance(elem, self.elem_class):
+            raise TypeError('Tipo inválido!')
+    def ordena(self, chave=None):
+        self.lista.sort(key=chave)
