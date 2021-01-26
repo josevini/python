63a373fd38c0d27c1436cb220dff8f993a089db0
José Vinícius Silva <ferreiravini468@gmail.com>
2021-01-26 16:45:39 -0300

    Update Nome.py

--- Introdução à Programação/capitulo10/Nome.py
+++ Introdução à Programação/capitulo10/Nome.py
@@ -1,3 +1,6 @@
+from functools import total_ordering
+
+@total_ordering
 class Nome:
     def __init__(self, nome):
         if nome is None or not nome.strip():
@@ -7,10 +10,13 @@ class Nome:
     def __str__(self):
         return self.nome
     def __repr__(self):
-        return f'<Classe {type(self).__name__} em 0x{id(self):x} em nome: {self.nome} chave: {self.chave}>'
+        return f'<Classe {type(self).__name__} em 0x{id(self):x} em Nome: {self.nome} Chave: {self.chave}>'
     def __eq__(self, outro):
         print('__eq__ chamado')
         return self.nome == outro.nome
     def __lt__(self, outro):
         print('__lt__ chamado')
         return self.nome < outro.nome
+    @staticmethod
+    def criaChave(nome):
+        return nome.strip().lower()
