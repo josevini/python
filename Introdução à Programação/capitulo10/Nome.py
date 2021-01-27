109b55a53fba4638f4ad735fd7153a6071a61743
José Vinícius Silva <ferreiravini468@gmail.com>
2021-01-27 20:39:47 -0300

    Update Nome.py

--- Introdução à Programação/capitulo10/Nome.py
+++ Introdução à Programação/capitulo10/Nome.py
@@ -3,14 +3,11 @@ from functools import total_ordering
 @total_ordering
 class Nome:
     def __init__(self, nome):
-        if nome is None or not nome.strip():
-            raise ValueError('Nome não pode ser nulo nem em branco')
         self.nome = nome
-        self.chave = nome.strip().lower()
     def __str__(self):
         return self.nome
     def __repr__(self):
-        return f'<Classe {type(self).__name__} em 0x{id(self):x} em Nome: {self.nome} Chave: {self.chave}>'
+        return f'<Classe {type(self).__name__} em 0x{id(self):x} em Nome: {self.__nome} Chave: {self.__chave}>'
     def __eq__(self, outro):
         print('__eq__ chamado')
         return self.nome == outro.nome
@@ -25,7 +22,7 @@ class Nome:
         if valor is None or not valor.strip():
             raise TypeError('Nome não pode ser nulo ou em branco.')
         self.__nome = valor
-        self.__nome = Nome.criaChave(valor)
+        self.__chave = Nome.criaChave(valor)
     @staticmethod
     def criaChave(nome):
         return nome.strip().lower()
