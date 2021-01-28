c6f47a9fef1617d4af9e86b481107000801b040e
José Vinícius Silva <ferreiravini468@gmail.com>
2021-01-28 19:47:13 -0300

    Update tipoTelefone.py

--- Introdução à Programação/capitulo10/tipoTelefone.py
+++ Introdução à Programação/capitulo10/tipoTelefone.py
@@ -5,4 +5,11 @@ class TipoTelefone:
     def __init__(self, tipo):
         self.tipo = tipo
     def __str__(self):
-        return f'({self.tipo})'
+        return f'({self.tipo}) '
+    def __eq__(self, other):
+        if other is None:
+            return False
+        else:
+            return self.tipo == other.tipo
+    def __lt__(self, other):
+        return self.tipo < other.tipo
