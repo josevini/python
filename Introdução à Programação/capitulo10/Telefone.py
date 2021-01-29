1e15ac8c91964c4ec417aab44aad45fb60caf55b
José Vinícius Silva <ferreiravini468@gmail.com>
2021-01-28 23:38:25 -0300

    Update Telefone.py

--- Introdução à Programação/capitulo10/Telefone.py
+++ Introdução à Programação/capitulo10/Telefone.py
@@ -2,3 +2,10 @@ class Telefone:
     def __init__(self, num, tipo=None):
         self.numero = num
         self.tipo = tipo
+    def __str__(self):
+        if self.tipo is not None:
+            tipo = self.tipo
+        else:
+            tipo = ''
+    def __eq__(self, other):
+        return self.numero == other.numero and ((self.tipo == other.tipo) or (self.tipo is None or other.tipo is None))
