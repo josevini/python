2e0890cfd91a50e2faacdfa812c932e4d4401392
José Vinícius Silva <ferreiravini468@gmail.com>
2021-02-07 19:37:57 -0300

    Update ex_11.1.py

--- Introdução à Programação/capitulo11/ex_11.1.py
+++ Introdução à Programação/capitulo11/ex_11.1.py
@@ -5,4 +5,6 @@ from contextlib import closing
 
 with sqlite3.connect('db/preco.db') as connection:
     with closing(connection.cursor()) as cursor:
-        cursor.execute('insert into produto values (?, ?, ?)', ('4', 'desktop', '2460.77'))
+        insert = 'insert into produto values (?, ?, ?)'
+        values = [('1', 'notebook', '1300.5'), ('2', 'celular', '900.5'), ('3', 'tablet', '320.6'), ('4', 'desktop', '2460.7')]
+        # cursor.executemany(insert, values)
