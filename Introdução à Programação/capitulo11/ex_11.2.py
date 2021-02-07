17bd74caeca85540f4184d0c71d65493b77a03ed
José Vinícius Silva <ferreiravini468@gmail.com>
2021-02-07 19:37:55 -0300

    Create ex_11.2.py

---
+++ Introdução à Programação/capitulo11/ex_11.2.py
@@ -0,0 +1,9 @@
+
+import sqlite3
+from contextlib import closing
+
+with sqlite3.connect('db/preco.db') as connection:
+    with closing(connection.cursor()) as cursor:
+        result = cursor.execute('select * from produto').fetchall()
+        for reg in result:
+            print(f'{reg[1]} {reg[2]}')
