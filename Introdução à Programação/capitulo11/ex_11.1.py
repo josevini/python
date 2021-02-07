338ea8d1df92be9333ab552d38b5f7fd850dbc46
José Vinícius Silva <ferreiravini468@gmail.com>
2021-02-07 19:19:38 -0300

    Update ex_11.1.py

--- Introdução à Programação/capitulo11/ex_11.1.py
+++ Introdução à Programação/capitulo11/ex_11.1.py
@@ -1,1 +1,11 @@
 # Exercício 11.1 - Livro
+
+import sqlite3
+from contextlib import closing
+
+with sqlite3.connect('db/preco.db') as connection:
+    with closing(connection.cursor()) as cursor:
+        # cursor.execute('insert into produto values (?, ?, ?)', ('3', 'tablet', '320.90'))
+        reg = cursor.execute('select * from produto')
+        while reg.fetchone():
+            print(reg.fetchone())
