68615344394e76b4b63c449f6d4db865920c9f0a
José Vinícius Silva <ferreiravini468@gmail.com>
2021-02-08 20:56:22 -0300

    Update ex_11.3.py

---
+++ Introdução à Programação/capitulo11/ex_11.3.py
@@ -0,0 +1,10 @@
+# Exercício 11.3 - Livro
+
+import sqlite3
+from contextlib import closing
+
+with sqlite3.connect('db/preco.db') as connection:
+    with closing(connection.cursor()) as cursor:
+        result = cursor.execute('select * from produto').fetchall()
+        for reg in result:
+            print(f'{reg[1]} {reg[2]}')
