a48e89505aa7cafc708f23ab6dc55345dbac7e4c
José Vinícius Silva <ferreiravini468@gmail.com>
2021-02-08 21:09:42 -0300

    Update ex_11.4.py

--- Introdução à Programação/capitulo11/ex_11.4.py
+++ Introdução à Programação/capitulo11/ex_11.4.py
@@ -3,6 +3,8 @@
 import sqlite3
 from contextlib import closing
 
+min = float(input('Digite um preço mínimo: '))
+max = float(input('Digite o preço máximo: '))
 with sqlite3.connect('db/preco.db') as connection:
     with closing(connection.cursor()) as cursor:
-        result = cursor.execute('select * from produto where').fetchall()
+        result = cursor.execute(f'select * from produto where preco >= {min} and preco <= {max}').fetchall()
