43c0d2a93bb069826b6e90a9128367eae75e40f7
José Vinícius Silva <ferreiravini468@gmail.com>
2021-02-21 00:05:37 -0300

    Update conversor.py

--- Meus exemplos/conversor.py
+++ Meus exemplos/conversor.py
@@ -1,15 +1,12 @@
 from func import *
 sair = 'n'
-
+print('---' * 10)
 while True:
     num = getBinaryNumber('Digite um valor na base 2: ')
-    if isBinary(num):
-        res = toDecimal(num)
-        print(f'Decimal: {int(res)}')
-    else:
-        print('ERRO')
+    res = toDecimal(num)
+    print(f'Decimal: {int(res)}')
     res = str(input('Quer sair? [S/N] ')).lower()
-    print('=-=' * 10)
+    print('---' * 10)
     if res == 's':
         break
     elif res == 'n':
